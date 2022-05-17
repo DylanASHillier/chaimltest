3e93d09c2b09a1193236804d6cfee5a3bdebe80e
Dylan Hillier <dylanslavnhillier@gmail.com>
2022-05-17 16:10:53 +0000

    latest

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from flask import Flask, request
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextGenerationPipeline
+from flask import Flask, request, json
 app = Flask(__name__)
 
 tokenizer = AutoTokenizer.from_pretrained("facebook/opt-30b")
 model = AutoModelForCausalLM.from_pretrained("facebook/opt-30b")
-model.eval()
+model.config.max_length=21
+
+pipeline = TextGenerationPipeline(model=model, tokenizer=tokenizer, device=-1)
+
+generation_params = {}
+# with open("generation_config.json") as f:
+#     generation_params = json.load(f)
+# generation_params=generation_params["generation_params"]
 
 @app.route('/', methods=['POST'])
 def infer():
@@ -12,22 +19,21 @@ def infer():
     Receives text input from post request and runs infernece on it.
     '''
     text = request.form['text']
-    tokenized_text = tokenizer(text, return_tensors="pt")
-    output = model.generate(**tokenized_text, 
-        max_length=100,
-        temperature=0.7,
-        do_sample=True,
-        top_p=0.9,
-
+    out = pipeline(
+        text, 
+        return_full_text=False,
+        **generation_params
     )
-
-    out =  tokenizer.decode(output[0])
-    print(out)
+    out = out[0]["generated_text"]
     return out
 
-# if __name__=='__main__':
-#     while True:
-#         text_input = input("Enter a sentence: ")
-#         tokenized_input = tokenizer(text_input, return_tensors="pt")
-#         output = model.generate(**tokenized_input)
-#         print(tokenizer.decode(output[0]))
+if __name__=='__main__':
+    while True:
+        text = input("Enter a sentence: ")
+        out = pipeline(
+            text, 
+            return_full_text=False,
+            **generation_params
+        )
+        out = out[0]["generated_text"]
+        print(out)
--- benchmark.py
+++ benchmark.py
@@ -4,21 +4,24 @@ from torchmetrics import BLEUScore
 import time
 import numpy as np
 import json
+import torch
+
+# test_dataset = 
 
 def run_benchmarks():
     # load generation params from json
     generation_params = {}
     with open("generation_config.json") as f:
-        generation_params = json.load(f)
+        generation_params = json.load(f)["generation_params"]
 
     models = [
         "facebook/opt-125m",
-        # "facebook/opt-350m",
+        "facebook/opt-350m",
         "facebook/opt-1.3b",
-        # "facebook/opt-2.7b",
-        # "facebook/opt-6.7b",
-        # "facebook/opt-13b",
-        # "facebook/opt-30b",
+        "facebook/opt-2.7b",
+        "facebook/opt-6.7b",
+        "facebook/opt-13b",
+        "facebook/opt-30b",
     ]
     metric = BLEUScore()
     # dataset = load_dataset("glue","qnli")["validation"]
@@ -26,7 +29,8 @@ def run_benchmarks():
     for model_string in models:
         model = AutoModelForCausalLM.from_pretrained(model_string)
         tokenizer = AutoTokenizer.from_pretrained(model_string)
-        pipeline = TextGenerationPipeline(model=model, tokenizer=tokenizer)
+        model.config.max_length=200
+        pipeline = TextGenerationPipeline(model=model, tokenizer=tokenizer, device=1)
         print(f"evaluating {model_string}")
         start = time.time()
         run_loop(pipeline, dataset, generation_params, metric)
@@ -39,19 +43,21 @@ def run_loop(pipeline, dataset, generation_params, metric):
     # questions = dataset["question"]
     # answers = dataset["sentence"]
     np.random.seed(123)
-    random_indices = np.random.choice(len(dataset), size=10, replace=False)
+    random_indices = np.random.choice(len(dataset), size=1, replace=False)
     for i in random_indices:
-        idx = len(dataset[i])//2
-        generation_params["max_new_tokens"]=idx//2
-        question, target = dataset[i][:idx], dataset[i][idx:]
+        text =dataset[i]
+        length = min(500,len(text))
+        idx = length//2
+        # generation_params["max_new_tokens"]=idx//2
+        question, target = dataset[i][:idx], dataset[i][idx:length]
         source = f"{question}"
-        answer = pipeline(source,return_full_text=False,generate_kwargs=generation_params)
+        answer = pipeline(source,return_full_text=False,**generation_params)
         answer = answer[0]["generated_text"]
-        # print(f"output: {output}")
+        # print(f"output: {answer}")
         # print(f"target: {target}")
         metric([answer],[[target]])
     scores = metric.compute()
-    print(scores)
+    print(f"scores were: {scores}")
         
 if __name__ == '__main__':
     run_benchmarks()
